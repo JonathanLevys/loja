4e0dec039b8c6117bdea9944a006def5a0f867b9
Jonathan Levy <jonathan.adm2012@gmail.com>
2022-06-16 19:03:37 -0300

    AULA 12 Verificando se a pg existe

--- model/Rotas.class.php
+++ model/Rotas.class.php
@@ -3,7 +3,15 @@
     class Rotas{
 
         static function get_pagina(){
-            if(isset($_GET['pag']));
+            if(isset($_GET['pag'])){
+                $pagina = $_GET['pag'] . 'php';
+                if(file_exists($pagina)){
+                    include $pagina;
+                }else {
+                    include'erro.php';  
+            
+            }  
+            }
         }
     }
 
